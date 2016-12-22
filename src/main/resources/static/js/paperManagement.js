012ee4b9cb8f656d3651e90e63cf895f724836bd
Me-Momo <add>
2016-12-22 10:04:25 +0800

    ''

--- src/main/resources/static/js/paperManagement.js
+++ src/main/resources/static/js/paperManagement.js
@@ -31,55 +31,82 @@ var dd_buttons = [{
 }];
 $(function() {
 
-    var teacher_id = $("#top strong").attr("id");
-    // 初始化 根据teacher_id 请求所有试题信息 如下
+    init();
 
-    // var data = {
-    //     'total': 2,
-    //     'rows': [{
-    //         paper_name: '数据库试题1',
-    //         paper_start: "11/17/2016 23:30:18",
-    //         paper_end: '11/17/2016 23:30:18',
-    //         paper_status: "1" //已发布
-    //     }, {
-    //         paper_name: '数据库试题2',
-    //         paper_start: "12/19/2016 23:30:18",
-    //         paper_end: '12/20/2016 23:30:18',
-    //         paper_status: "1" //已发布
-    //     }, {
-    //         paper_name: '数据库试题3',
-    //         paper_start: "",
-    //         paper_end: "",
-    //         paper_status: "0" //未发布
-    //     }]
-    // };
 
+
+});
+
+function init() {
     var paperStatus;
     var paperEnd;
     $.ajax({
-        url: '根据teacher_id 请求所有试题信息url',
-        type: "post",
-        data: {
-            teacher_id: teacher_id
+        url: '请求所有试题信息url',
+        type: "get",
+        data: null,
+        success: function(result) {
+            var $temp = $('#teacher_info')[0].innerHTML.trim();
+            $temp = $temp.replace('{TEACHERID}', result[0])
+                .replace('{TEACHERNAME}', result[1]);
+            $('#teacher_info').html($temp)
+            var data = result[2];
+            data.rows.forEach(function(item) {
+                paperStatus = item["paper_status"];
+                paperEnd = item["paper_end"];
+                if (paperStatus == "0") {
+                    paperStatus = "未发布...";
+                } else if (new Date(paperEnd).getTime() - Date.now() > 0) {
+                    paperStatus = "发布中...";
+                } else {
+                    paperStatus = "过期...";
+                };
+                item["paper_status"] = paperStatus;
+            });
+            $('#paper_dg').datagrid('loadData', data);
         },
-        success: function(data) {
-    data.rows.forEach(function(item) {
-        paperStatus = item["paper_status"];
-        paperEnd = item["paper_end"];
-        if (paperStatus == "0") {
-            paperStatus = "未发布...";
-        } else if (new Date(paperEnd).getTime() - Date.now() > 0) {
-            paperStatus = "发布中...";
-        } else {
-            paperStatus = "过期...";
-        };
-        item["paper_status"] = paperStatus;
-    });
-    $('#paper_dg').datagrid('loadData', data);
-    }
-   })
-
-});
+        error: function() {
+            //test
+            var data = {
+                'total': 2,
+                'rows': [{
+                    paper_name: '数据库试题1',
+                    paper_start: "11/17/2016 23:30:18",
+                    paper_end: '11/17/2016 23:30:18',
+                    paper_status: "1" //已发布
+                }, {
+                    paper_name: '数据库试题2',
+                    paper_start: "12/19/2016 23:30:18",
+                    paper_end: '12/22/2016 23:30:18',
+                    paper_status: "1" //已发布
+                }, {
+                    paper_name: '数据库试题3',
+                    paper_start: "",
+                    paper_end: "",
+                    paper_status: "0" //未发布
+                }]
+            };
+            var result = ['登录教师id', '登录教师名', data];
+            var $temp = $('#teacher_info')[0].innerHTML.trim();
+            $temp = $temp.replace('{TEACHERID}', result[0])
+                .replace('{TEACHERNAME}', result[1]);
+            $('#teacher_info').html($temp)
+            var data = result[2];
+            data.rows.forEach(function(item) {
+                paperStatus = item["paper_status"];
+                paperEnd = item["paper_end"];
+                if (paperStatus == "0") {
+                    paperStatus = "未发布...";
+                } else if (new Date(paperEnd).getTime() - Date.now() > 0) {
+                    paperStatus = "发布中...";
+                } else {
+                    paperStatus = "过期...";
+                };
+                item["paper_status"] = paperStatus;
+            });
+            $('#paper_dg').datagrid('loadData', data);
+        }
+    })
+}
 
 
 function openDg(id_dg) {
@@ -122,22 +149,6 @@ function previewPaper() {
         var paper_name = currentRow.paper_name;
         //根据试题名称请求试题内容
         openPanel();
-        // var data = {
-        //     'total': 2,
-        //     'rows': [{
-        //         question_type: "选择题",
-        //         question_title: '1、数据库（DB），数据库系统（DBS）和数据库管理系统（DBMS）之间的关系是（   　）。#A.　DBS包括DB和DBMS#B.　DBMS包括DB和DBS  #C.　DB包括DBS和DBMS  # D.　DBS就是DB，也就是DBMS',
-        //         question_id: "0001",
-        //         question_score: "2",
-        //         question_answer: "A"
-        //     }, {
-        //         question_type: "填空题",
-        //         question_title: '3、 用户选作元组标识的一个候选码为主码时，其属性不能取_____',
-        //         question_id: "0002",
-        //         question_score: "2",
-        //         question_answer: "空值"
-        //     }]
-        // };
         $.ajax({
             url: '根据试题名称请求试题内容url',
             type: "post",
@@ -145,6 +156,26 @@ function previewPaper() {
                 paper_name: paper_name
             },
             success: function(data) {
+                $('#paper_detail_dg').datagrid('loadData', data);
+            },
+            error: function() {
+                //test
+                var data = {
+                    'total': 2,
+                    'rows': [{
+                        question_type: "选择题",
+                        question_title: '1、数据库（DB），数据库系统（DBS）和数据库管理系统（DBMS）之间的关系是（   　）。#A.　DBS包括DB和DBMS#B.　DBMS包括DB和DBS  #C.　DB包括DBS和DBMS  # D.　DBS就是DB，也就是DBMS',
+                        question_id: "0001",
+                        question_score: "2",
+                        question_answer: "A"
+                    }, {
+                        question_type: "填空题",
+                        question_title: '3、 用户选作元组标识的一个候选码为主码时，其属性不能取_____',
+                        question_id: "0002",
+                        question_score: "2",
+                        question_answer: "空值"
+                    }]
+                };
                 $('#paper_detail_dg').datagrid('loadData', data);
             }
         })
@@ -169,26 +200,40 @@ function subscribePaper() {
             if (paperStart === "" || paperEnd === "") {
                 msgTipBox("时间不能为空");
             } else {
-                var data = {
-                    paper_name: paperName,
-                    paper_start: paperStart,
-                    paper_end: paperEnd
-                };
-                // 提交数据：data= {paper_name: "数据库试题1", paper_start: "11/23/2016 10:40:38", paper_end: "11/22/2016 10:40:40"}
-                // 服务器修改 该paper发布状态，返回提示 msg.success是否为true
+                var postdata = {
+      paper_name: paperName,
+      paper_start: paperStart,
+      paper_end: paperEnd
+  };
+
                 $.ajax({
                     url: '根据试题名称 发布试题',
                     type: "post",
+                    data:postdata,
                     success: function(msg) {
                         data["paper_status"] = "发布中";
                         if (msg.success) {
                             //提示发布成功
                             msgTipBox("发布成功,请关闭窗口");
+                            $('#dd').dialog({
+                                closed: true,
+                            });
                             $("#paper_dg").datagrid('updateRow', {
                                 index: currentIndex,
                                 row: data
                             });
                         }
+                    },
+                    error: function() {
+                        //test
+                        msgTipBox("发布成功,请关闭窗口");
+                        $('#dd').dialog({
+                            closed: true,
+                        });
+                        $("#paper_dg").datagrid('updateRow', {
+                            index: currentIndex,
+                            row: data
+                        });
                     }
                 }); //ajax end
 
@@ -249,22 +294,33 @@ function modifyQuestion() {
                 row['question_title'] += $item.val() + "#";
             });
 
-            // 提交修改试题申请
-            // 提交json数据
-            // 浏览器提交数据：row={question_answer: "答案答案答案",question_id: "0002",question_score: "2",question_title: "3、 用户选作元组标识的一个候选码为主码时，其属性不能取_____####"}
-            // 服务器返回提示信息：msg.success是否为true
+            var postdata = { 'paper_name': paper_name, 'question': row }
             $.ajax({
                 url: "修改试题url",
                 type: "post",
-                data: row,
+                data: postdata,
                 success: function(msg) {
                     if (msg.success) {
+                        $('#dd').dialog({
+                            closed: true,
+                        });
                         msgTipBox("修改成功");
                         $("#paper_detail_dg").datagrid('updateRow', {
                             index: currentIndex2,
                             row: row
                         });
                     }
+                },
+                error: function() {
+                    //test
+                    $('#dd').dialog({
+                        closed: true,
+                    });
+                    msgTipBox("修改成功");
+                    $("#paper_detail_dg").datagrid('updateRow', {
+                        index: currentIndex2,
+                        row: row
+                    });
                 }
             }); // ajax end
         });
@@ -278,9 +334,8 @@ function modifyQuestion() {
 };
 
 function editPaper() {
-    // body...
+    //To-do
 }
-
 // 附加功能 日历
 $('#calender').calendar({
     current: new Date()
--- src/main/resources/static/js/paperMark.js
+++ src/main/resources/static/js/paperMark.js
@@ -1,21 +1,61 @@
 var currentPaper = null; //全局变量
-
 $(function() {
-    // 根据teacher_id请求所有未评阅的试卷信息
-    var teacher_id = $("#top strong").attr("id");
+    //初始化
+    init();
+    // 请求对应试题 学生答卷
+    $("#paper_list a").bind("click", function() {
+        $("#paper_list a").removeClass('active');
+        $(this).addClass('active');
+        var paper_id = $(this).attr("id");
+        currentPaper = paper_id;
+        // 获取试题
+        getAnswerPaper(currentPaper, 1);
+    });
+    $('#pp').pagination({
+        onSelectPage: function(pageNumber, pageSize) {
+            $(this).pagination('loading');
+            getAnswerPaper(currentPaper, pageNumber);
+            $(this).pagination('loaded');
+        }
+    });
+
+});
+
+function init() {
     $.ajax({
         url: null,
-        type: "post",
-        data: { teacher_id: teacher_id },
-        // var data = [{
-        //     paper_id: "paper_id1",
-        //     paper_name: "数据库试题1"
-        // }, {
-        //     paper_id: "paper_id2",
-        //     paper_name: "数据库试题2"
-        // }];
-
-        success: function(data) {
+        type: "get",
+        data: null,
+        success: function(result) {
+            var $temp = $('#teacher_info')[0].innerHTML.trim();
+            $temp = $temp.replace('{TEACHERID}', result[0])
+                .replace('{TEACHERNAME}', result[1]);
+            $('#teacher_info').html($temp);
+            var data = result[2];
+            var $ul = $("#paper_list ul");
+            $ul.find("li").remove();
+            for (var i = 0; i < data.length; i++) {
+                var $template = $("#template_paper_list")[0].innerHTML.trim();
+                $template = $template.replace('{PAPERID}', data[i]['paper_id'])
+                    .replace('{PAPERNAME}', data[i]['paper_name']);
+                $ul.append($template);
+            }
+        },
+        error: function() {
+            //test
+            var result = ['登录教师id', '登录教师名', data];
+            var data = [{
+                paper_id: "paper_id1",
+                paper_name: "数据库试题1"
+            }, {
+                paper_id: "paper_id2",
+                paper_name: "数据库试题2"
+            }];
+            var $temp = $('#teacher_info')[0].innerHTML.trim();
+            $temp = $temp.replace('{TEACHERID}', result[0])
+                .replace('{TEACHERNAME}', result[1]);
+            $('#teacher_info').html($temp);
+            var data = result[2];
             var $ul = $("#paper_list ul");
             $ul.find("li").remove();
             for (var i = 0; i < data.length; i++) {
@@ -26,72 +66,54 @@ $(function() {
             }
         }
     }); // ajax end
-});
+}
 
-$(function() {
-    // 请求对应试题 学生答卷
-    $("#paper_list a").bind("click", function() {
-        $("#paper_list a").removeClass('active');
-        $(this).addClass('active');
-        var paper_id = $(this).attr("id");
-        currentPaper = paper_id;
-        // 获取试题总页数
-        getPaperTotalNum(currentPaper, function(num) {
-            if (num > 0) {
-                getPaper(currentPaper, 1);
-                var total = num; //代表该份试题参与学生人数
-                $('#pp').pagination({
-                    total: total,
-                    pageList: [1],
-                    pageSize: 1
-                });
-            } else {
-                alert("该份试题，暂时没有参加考试！")
-            }
-        });
-    });
 
-    $('#pp').pagination({
-        onSelectPage: function(pageNumber, pageSize) {
-            $(this).pagination('loading');
-            getPaper(currentPaper, pageNumber);
-            $(this).pagination('loaded');
-        }
-    });
-
-});
-
-// var data = {
-//            "student_id": '201430560243',
-//            "student_name": '廖晓娟',
-//            "student_paper": [
-//                ['题目1-id','题目1', '答案1', '5(分值)'],
-//                ['题目2-id','题目2','答案2', '5']
-//            ],
-//            "student_score": 0
-//        };
 // 根据paper_id 和pageNumber 发起请求 第pageNumber份试卷 (包括学生id 学生姓名 学生答案 学生成绩-判断是否已经评分)
-function getPaper(paper_id, index) {
+function getAnswerPaper(paper_id, index) {
     $.ajax({
         url: '根据paper_id,请求第index份试题',
         data: { 'paper_id': paper_id, 'index': index },
         type: 'post',
-        success: function(data) {
-            render(data.student_id, data.student_name, data.student_paper, data.student_score);
+        success: function(result) {
+            var num = result[0];
+            var data = result[1];
+            if (num > 0) {
+                $('#pp').pagination({
+                    total: num, //代表该份试题参与学生人数
+                    pageList: [1],
+                    pageSize: 1
+                });
+                render(data.student_id, data.student_name, data.student_paper, data.student_score);
+            } else {
+                alert("该份试题，暂时没有参加考试！")
+            }
+        },
+        error: function() {
+            var data = {
+                "student_id": '201430560243',
+                "student_name": '廖晓娟',
+                "student_paper": [
+                    ['题目1-id', '题目1', '答案1', '5(分值)'],
+                    ['题目2-id', '题目2', '答案2', '5']
+                ],
+                "student_score": 0
+            };
+            var result = [1, data];
+            var num = result[0];
+            var data = result[1];
+            if (num > 0) {
+                $('#pp').pagination({
+                    total: num, //代表该份试题参与学生人数
+                    pageList: [1],
+                    pageSize: 1
+                });
+                render(data.student_id, data.student_name, data.student_paper, data.student_score);
+            }
         }
     });
 }
 
-function getPaperTotalNum(paper_id, callback) {
-    $.ajax({
-        url: '根据paper_id,请求该份试题参与考试学生人数',
-        type: 'post',
-        data: { 'paper_id': paper_id },
-        success: function(num) {
-            callback(num);
-        }
-    })
-}
 
 function render(student_id, student_name, paper, score) {
 
@@ -125,7 +147,6 @@ function addTotalScore() {
         var target = $(event.target).data('target');
         var max = Number($(target).data('max'));
         var score = $(target).val();
-        console.log(Number(score) > max)
         if (!/\d+/.test(score)) {
             alert('请给一个合法的分数')
         } else if (Number(score) > max) {
@@ -165,9 +186,6 @@ function submitScore(student_id) {
         }
     })
 }
-
-
-
 
 
 // 效果添加
